9f672030c3141e34d8173d39971ead8a167ba2c5
Cole-Boothe <coleboothe3@gmail.com>
2025-11-18 09:56:32 -0800

    Delete Sophomore year/Java directory

--- Sophomore year/Java/p8_C00558477/Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"mybinarytreeexample","l":"MyBinaryTree"},{"p":"mybinarytreeexample","l":"MyBinaryTreeExample"},{"p":"mybinarytreeexample","l":"MyBinaryTree.Node"}]
--- Sophomore year/Java/p9_C00558477/Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"heapsortexample","l":"Heap"},{"p":"heapsortexample","l":"HeapSort"}]
